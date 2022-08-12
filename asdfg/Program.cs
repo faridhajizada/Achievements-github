998d37108dd5c7baae4498f83fd6bdb16965dc9b
Farid0558475484 <faridmh@code.edu.az>
2022-08-12 10:21:13 +0400

    qwerty

---
+++ asdfg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asdfg
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
